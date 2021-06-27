88db70cd727799f1cd40d808ac046ead5c3d3e94
resueman14 <56014045+resueman14@users.noreply.github.com>
2021-06-27 16:19:01 +0500

    ASYNC AWAIT CALBACK PROMISE karoch

---
+++ models/item.js
@@ -0,0 +1,54 @@
+const { v4: uuidv4 } = require('uuid')
+const fs = require('fs')
+const path = require('path')
+const { rejects } = require('assert')
+class Item {
+    constructor(title, price, img){
+        this.title = title
+        this.price = price
+        this.img = img
+        this.id = uuidv4()
+    }
+
+    async save() {
+        const items = await Item.getAll()
+        items.push(this.toJSON())
+        return new Promise ((resolve, reject)=>{
+            fs.writeFile(
+                path.join(__dirname,'..','data','items.json'),
+                JSON.stringify(items),
+                (err) => {
+                    if (err) { reject(err) 
+                    } else { resolve(
+
+                    )}
+                }
+
+            )
+        })
+    }
+
+    toJSON() {
+        return {
+            title: this.title,
+            price: this.price,
+            img: this.img,
+            id: this.id
+        }
+    }
+
+    static getAll() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(
+                path.join(__dirname,'..','data','items.json'), 
+                'utf-8', 
+                (err, content) => {
+                    if (err) { reject(err)} 
+                    else {resolve(JSON.parse(content))}
+                }
+            )
+        })
+    }
+}
+
+module.exports = Item
--- routes/add.js
+++ routes/add.js
@@ -1,13 +1,16 @@
 const {Router} = require('express')
 const router = Router()
+const Item = require('../models/item')
 
 router.get('/', (req,res) => {
     res.status(200)
     res.render('add',{title:'Добавление нового элемента', isAdd: true})
 })
 
-router.post('/', (req,res)=>{
-    console.log(req.body)
+router.post('/', async (req,res)=>{
+    
+    const item = new Item(req.body.title, req.body.price, req.body.img)
+    await item.save()
     res.redirect('/items')
 })
 
