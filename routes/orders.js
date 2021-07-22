83fb5c0f1d5dcf9e483f5e7b5c98760654b6ae4f
resueman14 <56014045+resueman14@users.noreply.github.com>
2021-07-22 15:15:46 +0500

    orders

--- routes/orders.js
+++ routes/orders.js
@@ -3,10 +3,23 @@ const Order = require('../models/order')
 const router = Router()
 
 router.get('/', async (req,res)=>{
-    res.render('orders',{
-        isOrder: true,
-        title: 'Заказы'
-    })
+    try {
+        const orders = await Order.find({'user.userId':req.user._id})
+            .populate('user.userId')
+        res.render('orders',{
+            orders: orders.map(o=>{return {
+                        ...o.toJSON(), 
+                        price: o.items.reduce((total, i)=>{
+                            return total += i.count * i.item.price
+                    }, 0)
+                }
+            }),
+            isOrder: true,
+            title: 'Заказы'
+        })
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 router.post('/', async (req, res) => {
