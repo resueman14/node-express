5a1d55fdf745e130de6d657c49be5bc352770d3f
resueman14 <56014045+resueman14@users.noreply.github.com>
2021-08-01 01:38:17 +0500

    password crypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const bcrypt = require('bcryptjs')
 const router = Router()
 const User = require('../models/user')
 
@@ -17,15 +18,54 @@ router.get('/logout', async (req,res)=>{
 })
 
 router.post('/login', async (req,res)=>{
-    const user = await User.findById("60f5a2485921081d08d58efa")
-    req.session.user = user
-    req.session.isAuthenticated = true
-    req.session.save(err=>{
-        if (err){
-            throw err
-        }  
-        res.redirect('/')
-    })
+    try {
+        const {email, password} = req.body
+        const candidate = await User.findOne({email})
+        if(candidate){
+            const areSame = await bcrypt.compare(password, candidate.password)
+            if(areSame){
+                req.session.user = candidate
+                req.session.isAuthenticated = true
+                req.session.save(err=>{
+                    if (err){
+                        throw err
+                    }  
+                    res.redirect('/')
+                })
+            } else {
+                res.redirect('/auth#login')
+            }
+        }else{
+            res.redirect('/auth#login')
+        }
+    } catch(e){
+        console.log(e)
+    }
+
+
+   
+})
+
+router.post('/register', async (req,res)=>{
+    try {
+        const {name,email,password, repeat} = req.body
+        const candidate = await User.findOne({email})
+        if(candidate){
+            res.redirect('/auth#register')
+        } else {
+            const hashPassword = await bcrypt.hash(password,10)
+            const user = new User({
+                email:email, 
+                name:name,
+                password:hashPassword,
+                cart:{items:[]}
+            })
+          await user.save()  
+          res.redirect('/auth#login')
+        } 
+    } catch (err) {
+        console.log(err)
+    }
    
 })
 
